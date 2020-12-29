f5cedd4ad2b52c4fc3f8b9d801b6d7e4a865cf9d
Douglas Hammarstam <dogge20@hotmail.com>
2020-12-29 02:31:33 +0100

    user auth

--- app.go
+++ app.go
@@ -1,184 +1,190 @@
 package main
 
 import (
-    "database/sql"
-    "fmt"
-    "log"
+	"database/sql"
+	"fmt"
+	"log"
 
-    "encoding/json"
-    "net/http"
-    "strconv"
+	"encoding/json"
+	"net/http"
+	"strconv"
 
-    "github.com/gorilla/mux"
-    _ "github.com/lib/pq"
-    "github.com/rs/cors"
+	"github.com/gorilla/mux"
+	_ "github.com/lib/pq"
+	"github.com/rs/cors"
 )
 
 type App struct {
-    Router *mux.Router
-    DB     *sql.DB
+	Router *mux.Router
+	DB     *sql.DB
 }
 
-
 func (a *App) Initialize(user, password, dbname string) {
-    connectionString :=
-        fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable", user, password, dbname)
+	connectionString :=
+		fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable", user, password, dbname)
 
-    var err error
-    a.DB, err = sql.Open("postgres", connectionString)
-    if err != nil {
-        log.Fatal(err)
+	var err error
+	a.DB, err = sql.Open("postgres", connectionString)
+	if err != nil {
+		log.Fatal(err)
 	}
 
 	a.Router = mux.NewRouter()
 
 	a.initializeRoutes()
-	
 
 }
 
 func (a *App) Run(addr string) {
 
-
-    c := cors.New(cors.Options{
-        AllowedOrigins: []string{"http://localhost:3000"},
-        AllowCredentials: true,
+	c := cors.New(cors.Options{
+		AllowedOrigins:   []string{"http://localhost:3000"},
+		AllowCredentials: true,
 	})
 
 	handler := c.Handler(a.Router)
-	
 
-    log.Fatal(http.ListenAndServe(":8010", handler))
+	log.Fatal(http.ListenAndServe(":8010", handler))
 }
 
 func (a *App) getUser(w http.ResponseWriter, r *http.Request) {
-    vars := mux.Vars(r)
-    id, err := strconv.Atoi(vars["id"])
-    if err != nil {
-        respondWithError(w, http.StatusBadRequest, "Invalid User ID")
-        return
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid User ID")
+		return
 	}
-	
 
 	p := user{ID: id}
 	fmt.Println(id)
 
-    if err := p.getUser(a.DB); err != nil {
-        switch err {
-        case sql.ErrNoRows:
-            respondWithError(w, http.StatusNotFound, "User not found")
-        default:
-            respondWithError(w, http.StatusInternalServerError, err.Error())
-        }
-        return
-    }
+	if err := p.getUser(a.DB); err != nil {
+		switch err {
+		case sql.ErrNoRows:
+			respondWithError(w, http.StatusNotFound, "User not found")
+		default:
+			respondWithError(w, http.StatusInternalServerError, err.Error())
+		}
+		return
+	}
 
-    respondWithJSON(w, http.StatusOK, p)
+	respondWithJSON(w, http.StatusOK, p)
 }
 
-
 func respondWithError(w http.ResponseWriter, code int, message string) {
-    respondWithJSON(w, code, map[string]string{"error": message})
+	respondWithJSON(w, code, map[string]string{"error": message})
 }
 
 func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 	response, _ := json.Marshal(payload)
-	
-    w.Header().Set("Content-Type", "application/json")
-    w.WriteHeader(code)
-    w.Write(response)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(response)
 }
 
 func (a *App) getUsers(w http.ResponseWriter, r *http.Request) {
-    count, _ := strconv.Atoi(r.FormValue("count"))
-    start, _ := strconv.Atoi(r.FormValue("start"))
+	count, _ := strconv.Atoi(r.FormValue("count"))
+	start, _ := strconv.Atoi(r.FormValue("start"))
 
-    if count > 10 || count < 1 {
-        count = 1000
-    }
-    if start < 0 {
-        start = 0
-    }
+	if count > 10 || count < 1 {
+		count = 1000
+	}
+	if start < 0 {
+		start = 0
+	}
 
-    users, err := getUsers(a.DB, start, count)
-    if err != nil {
-        respondWithError(w, http.StatusInternalServerError, err.Error())
-        return
-    }
+	users, err := getUsers(a.DB, start, count)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
 
-    respondWithJSON(w, http.StatusOK, users)
+	respondWithJSON(w, http.StatusOK, users)
 }
-
 
 func (a *App) createUser(w http.ResponseWriter, r *http.Request) {
 	var p user
-
-
-
-	
-    decoder := json.NewDecoder(r.Body)
-    if err := decoder.Decode(&p); err != nil {
-        respondWithError(w, http.StatusBadRequest, "Invalid request payload")
-        return
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(&p); err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid request payload")
+		return
 	}
 	defer r.Body.Close()
 
-    if err := p.createUser(a.DB); err != nil {
-        respondWithError(w, http.StatusInternalServerError, err.Error())
-        return
+	if err := p.createUser(a.DB); err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
 	}
 
-    respondWithJSON(w, http.StatusCreated, p)
+	respondWithJSON(w, http.StatusCreated, p)
 }
 
-
 func (a *App) updateUser(w http.ResponseWriter, r *http.Request) {
-    vars := mux.Vars(r)
-    id, err := strconv.Atoi(vars["id"])
-    if err != nil {
-        respondWithError(w, http.StatusBadRequest, "Invalid user ID")
-        return
-    }
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid user ID")
+		return
+	}
 
-    var p user
-    decoder := json.NewDecoder(r.Body)
-    if err := decoder.Decode(&p); err != nil {
-        respondWithError(w, http.StatusBadRequest, "Invalid resquest payload")
-        return
-    }
-    defer r.Body.Close()
-    p.ID = id
+	var p user
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(&p); err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid resquest payload")
+		return
+	}
+	defer r.Body.Close()
+	p.ID = id
 
-    if err := p.updateUser(a.DB); err != nil {
-        respondWithError(w, http.StatusInternalServerError, err.Error())
-        return
-    }
+	if err := p.updateUser(a.DB); err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
 
-    respondWithJSON(w, http.StatusOK, p)
+	respondWithJSON(w, http.StatusOK, p)
 }
 
 func (a *App) deleteUser(w http.ResponseWriter, r *http.Request) {
-    vars := mux.Vars(r)
-    id, err := strconv.Atoi(vars["id"])
-    if err != nil {
-        respondWithError(w, http.StatusBadRequest, "Invalid User ID")
-        return
-    }
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid User ID")
+		return
+	}
 
-    p := user{ID: id}
-    if err := p.deleteUser(a.DB); err != nil {
-        respondWithError(w, http.StatusInternalServerError, err.Error())
-        return
-    }
+	p := user{ID: id}
+	if err := p.deleteUser(a.DB); err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
 
-    respondWithJSON(w, http.StatusOK, map[string]string{"result": "success"})
+	respondWithJSON(w, http.StatusOK, map[string]string{"result": "success"})
 }
 
+func (a *App) authenticateUser(w http.ResponseWriter, r *http.Request) {
+	var p user
+	decoder := json.NewDecoder(r.Body)
+
+	if err := decoder.Decode(&p); err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid request payload")
+		return
+	}
+	defer r.Body.Close()
+
+	if err := p.authenticateUser(a.DB); err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, map[string]string{"result": "success"})
+}
 
 func (a *App) initializeRoutes() {
-    a.Router.HandleFunc("/users", a.getUsers).Methods("GET")
-    a.Router.HandleFunc("/user", a.createUser).Methods("POST")
-    a.Router.HandleFunc("/user/{id:[0-9]+}", a.getUser).Methods("GET")
-    a.Router.HandleFunc("/user/{id:[0-9]+}", a.updateUser).Methods("PUT")
-    a.Router.HandleFunc("/user/{id:[0-9]+}", a.deleteUser).Methods("DELETE")
+	a.Router.HandleFunc("/users", a.getUsers).Methods("GET")
+	a.Router.HandleFunc("/user", a.createUser).Methods("POST")
+	a.Router.HandleFunc("/user/{id:[0-9]+}", a.getUser).Methods("GET")
+	a.Router.HandleFunc("/user/{id:[0-9]+}", a.updateUser).Methods("PUT")
+	a.Router.HandleFunc("/user/{id:[0-9]+}", a.deleteUser).Methods("DELETE")
+	a.Router.HandleFunc("/auth", a.authenticateUser).Methods("POST")
 }
