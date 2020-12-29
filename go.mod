4c7f1f735de31af8ae2771d0d734ab2db1dd5b43
Douglas Hammarstam <dogge20@hotmail.com>
2020-12-29 02:31:24 +0100

    user auth

--- go.mod
+++ go.mod
@@ -6,4 +6,5 @@ require (
 	github.com/gorilla/mux v1.8.0
 	github.com/lib/pq v1.9.0
 	github.com/rs/cors v1.7.0
+	golang.org/x/crypto v0.0.0-20201221181555-eec23a3978ad
 )
--- user.go
+++ user.go
@@ -3,72 +3,116 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"golang.org/x/crypto/bcrypt"
 	"strconv"
-
 )
 
 type user struct {
-    ID    int     `json:"id"`
-    Username  string  `json:"username"`
-    Password string `json:"password"`
+	ID       int    `json:"id"`
+	Username string `json:"username"`
+	Password string `json:"password"`
 }
 
 func (p *user) getUser(db *sql.DB) error {
-	fmt.Println(db.QueryRow("SELECT username, password FROM users WHERE id="+strconv.Itoa(p.ID)).Scan())
-    return db.QueryRow("SELECT username, password FROM users WHERE id="+strconv.Itoa(p.ID)).Scan(&p.Username, &p.Password)
+	fmt.Println(db.QueryRow("SELECT username, password FROM users WHERE id=" + strconv.Itoa(p.ID)).Scan())
+	return db.QueryRow("SELECT username, password FROM users WHERE id="+strconv.Itoa(p.ID)).Scan(&p.Username, &p.Password)
+}
+
+func (p *user) authenticateUser(db *sql.DB) error {
+
+	fmt.Println(p.Username)
+
+	var user user
+
+	row := db.QueryRow(
+		"SELECT password FROM users where username=$3 LIMIT $1 OFFSET $2",
+		1, 0, p.Username)
+
+	err := row.Scan(&user.Password)
+	switch err {
+	case sql.ErrNoRows:
+		fmt.Println("No rows were returned!")
+		return err
+	case nil:
+		fmt.Println(user)
+
+	default:
+		panic(err)
+	}
+
+	fmt.Println(user.Password)
+
+	if err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(p.Password)); err != nil {
+
+	}
+
+	return err
+
 }
 
 func (p *user) updateUser(db *sql.DB) error {
-    _, err :=
-        db.Exec("UPDATE user SET username=$1, password=$2 WHERE id=$3",
-            p.Username, p.Password, p.ID)
+	_, err :=
+		db.Exec("UPDATE user SET username=$1, password=$2 WHERE id=$3",
+			p.Username, p.Password, p.ID)
 
-    return err
+	return err
 }
 
 func (p *user) deleteUser(db *sql.DB) error {
-    _, err := db.Exec("DELETE FROM users WHERE id=$1", p.ID)
+	_, err := db.Exec("DELETE FROM users WHERE id=$1", p.ID)
 
-    return err
+	return err
 }
 
 func (p *user) createUser(db *sql.DB) error {
 
-	fmt.Println("USER!!!!!!!!!!!!!!! " + p.Username)
+	hashedPassword := hashPassword(p.Password)
 
+	err := db.QueryRow(
+		"INSERT INTO users(username, password) VALUES($1, $2) RETURNING id",
+		p.Username, hashedPassword).Scan(&p.ID)
 
+	if err != nil {
+		return err
+	}
 
-    err := db.QueryRow(
-        "INSERT INTO users(username, password) VALUES($1, $2) RETURNING id",
-        p.Username, p.Password).Scan(&p.ID)
+	return nil
+}
 
-    if err != nil {
-        return err
-    }
+func hashPassword(password string) []byte {
 
-    return nil
+	passwordByteArr := []byte(password)
+	hashedPassword, err := bcrypt.GenerateFromPassword(passwordByteArr, bcrypt.DefaultCost)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(hashedPassword))
+
+	err = bcrypt.CompareHashAndPassword(hashedPassword, passwordByteArr)
+
+	return hashedPassword
 }
 
 func getUsers(db *sql.DB, start, count int) ([]user, error) {
-    rows, err := db.Query(
-        "SELECT id, username, password FROM users LIMIT $1 OFFSET $2",
-        count, start)
+	rows, err := db.Query(
+		"SELECT id, username, password FROM users LIMIT $1 OFFSET $2",
+		count, start)
 
-    if err != nil {
-        return nil, err
-    }
+	if err != nil {
+		return nil, err
+	}
 
-    defer rows.Close()
+	defer rows.Close()
 
-    users := []user{}
+	users := []user{}
 
-    for rows.Next() {
-        var p user
-        if err := rows.Scan(&p.ID, &p.Username, &p.Password); err != nil {
-            return nil, err
-        }
-        users = append(users, p)
-    }
+	for rows.Next() {
+		var p user
+		if err := rows.Scan(&p.ID, &p.Username, &p.Password); err != nil {
+			return nil, err
+		}
+		users = append(users, p)
+	}
 
-    return users, nil
+	return users, nil
 }
